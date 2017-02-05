67a3b1e8b357c47902b0ee6b515a324676499172
EC2 Default User <ec2-user@ip-172-31-28-191.ec2.internal>
2017-02-05 05:19:59 +0000

    did some stuff w trees, finished lasso; gonna do more trees

--- modeling/helper_functions.py
+++ modeling/helper_functions.py
@@ -17,3 +17,16 @@ def dummify_cols_and_baselines(df, cols):
 def make_alphas(start, stop):
 	lst = pd.np.logspace(start, stop, pd.np.abs(stop - start) + 1)
 	return [[i] for i in lst]
+
+
+def replace(group, stds):
+    # http://stackoverflow.com/questions/29740216/remove-outliers-3-std-and-replace-with-np-nan-in-python-pandas
+    group[pd.np.abs(group - group.median()) > stds * group.std()] = pd.np.nan
+    return group
+
+
+def remove_outliers_by_type(df, y_col, std_devs=3):
+    group_column = 'TYPE'
+    df = df.copy()
+    df.loc[:, y_col] = df[[y_col, 'TYPE']].groupby(group_column).transform(lambda g: replace(g, std_devs))
+    return df.dropna(subset=[y_col])
