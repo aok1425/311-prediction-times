13827182c73a97ec361be4b9815e80f0c07f1ae3
aok1425 <aok1425@gmail.com>
2017-02-10 12:04:19 -0800

    cleaning up preprocessing folder

---
+++ preprocessing/make_csv_for_tableau.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os, sys
+sys.path.append(os.path.join(os.path.dirname('.'), "../preprocessing"))
+from transform_for_num_issues_pred import main as transform_dataset, group_table, removing_cols
+
+df_orig = pd.read_pickle('../data/data_from_remove_from_dataset.pkl')
+df = transform_dataset(df_orig)
+df.to_csv('../data/data_for_tableau.csv', index=False, encoding='utf-8')
