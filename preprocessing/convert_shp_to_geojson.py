d899e8574774c26fa66250af7d32597a22e922f2
aok1425 <aok1425@gmail.com>
2017-02-18 23:00:50 -0800

    updated geojson

--- preprocessing/convert_shp_to_geojson.py
+++ preprocessing/convert_shp_to_geojson.py
@@ -2,17 +2,19 @@
 
 import shapefile
 # read the shapefile
-reader = shapefile.Reader("data/shape-files/cb_2015_25_bg_500k.shp")
+reader = shapefile.Reader("../data/shape-files/cb_2015_25_bg_500k.shp")
 fields = reader.fields[1:]
 field_names = [field[0] for field in fields]
 buffer = []
+
 for sr in reader.shapeRecords():
     atr = dict(zip(field_names, sr.record))
     if atr['COUNTYFP'] == '025':
+        new_atr = {'tract_and_block_group': atr['GEOID'][-7:]}
         geom = sr.shape.__geo_interface__
-        buffer.append(dict(type="Feature", geometry=geom, properties=atr)) 
+        buffer.append(dict(type="Feature", geometry=geom, properties=new_atr)) 
 
 from json import dumps
-geojson = open("data/shape-files/cb_2015_25_bg_500k.geojson", "w")
+geojson = open("../data/shape-files/cb_2015_25_bg_500k.geojson", "w")
 geojson.write(dumps({"type": "FeatureCollection", "features": buffer}, indent=2) + "\n")
 geojson.close()        
