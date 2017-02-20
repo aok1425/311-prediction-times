455e56b3c26baa5d3636e30943a99f572322e8c2
Alexander Ko <aok1425@gmail.com>
2017-02-20 22:08:57 +0000

    Q1 removing outliers doesn't help much :(

--- preprocessing/transform_for_num_issues_pred.py
+++ preprocessing/transform_for_num_issues_pred.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 BLOCK_GROUP_BLACKLIST = ["9807001", "9818001", "0303003", "0701018", "9811003"] # these are parks or South Station
 OUTLIERS_COMMERCIAL_INDUSTRIAL = ['0102034', '0107013', '0512001', '0612002', '0701012', '1101033', '9812021']
 OUTLIERS_LOW_POP = ['0005024', '0008032', '0103002', '0104051']
+OUTLIERS_POP_0 = [u'9811002', u'9815011', u'9810001', u'9811001', u'9816001', u'9817001', u'9812011', u'9815021']
 
 
 def dummify(df, column, keep_baseline=False):
@@ -122,7 +123,7 @@ def assert_no_duplicates(df, **kwargs):
 
 
 def drop_outliers(df, **kwargs):
-    outliers = OUTLIERS_LOW_POP + OUTLIERS_COMMERCIAL_INDUSTRIAL + BLOCK_GROUP_BLACKLIST
+    outliers = OUTLIERS_LOW_POP + OUTLIERS_COMMERCIAL_INDUSTRIAL + BLOCK_GROUP_BLACKLIST + OUTLIERS_POP_0
     df1 = df[~df.tract_and_block_group.isin(outliers)]
     assert df.shape != df1.shape
     return df1
